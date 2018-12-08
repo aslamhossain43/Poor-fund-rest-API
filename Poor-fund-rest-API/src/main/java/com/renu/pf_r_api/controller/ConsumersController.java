91d23272dc4054be1fbe278eae2ec878ebff3a45
Atif Aslam <aslam.renu.soft43@gmail.com>
2018-12-08 14:04:09 -0800

    Update completed

--- Poor-fund-rest-API/src/main/java/com/renu/pf_r_api/controller/ConsumersController.java
+++ Poor-fund-rest-API/src/main/java/com/renu/pf_r_api/controller/ConsumersController.java
@@ -1,48 +1,56 @@
 package com.renu.pf_r_api.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.renu.pf_r_api.fileupload.FileUpload;
+import com.renu.pf_r_api.models.Consumers;
+import com.renu.pf_r_api.repositories.ConsumersRepository;
+
 @CrossOrigin("*")
 @RestController
-@RequestMapping(value="/consumers")
+@RequestMapping(value = "/consumers")
 public class ConsumersController {
-private static final Logger LOGGER=LoggerFactory.getLogger(ConsumersController.class);
-@Autowired
- FileUpload fileUpload;
-private static List<String> files = new ArrayList<String>();
-	@PostMapping(value="/addFile")
-	public static ResponseEntity<String>addConsumersFile(@RequestParam("piFile") MultipartFile piFile) {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ConsumersController.class);
+	@Autowired
+	FileUpload fileUpload;
+	@Autowired
+	ConsumersRepository consumersRepository;
+	public String piCode = null;
+	public String apiCode = null;
+
+	@PostMapping(value = "/addFile")
+	public ResponseEntity<?> addConsumersFile(@RequestParam("piFile") MultipartFile piFile,
+			@RequestParam("apiFile") MultipartFile apiFile) {
 		LOGGER.info("From class ConsumersController ,method : ResponseEntity<String>addConsumersFile()");
-		String message = "";
 		try {
-			FileUpload.piFileUpload(piFile);
-			files.add(piFile.getOriginalFilename());
- 
-			message = "You successfully uploaded " + piFile.getOriginalFilename() + "!";
-			return ResponseEntity.status(HttpStatus.OK).body(message);
+			FileUpload.fileUpload(piFile, this.piCode, apiFile, this.apiCode);
+			this.piCode = null;
+			this.apiCode = null;
+			return ResponseEntity.ok().body("file is uploaded !");
 		} catch (Exception e) {
-			message = "FAIL to upload " + piFile.getOriginalFilename() + "!";
-			return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(message);
+			return ResponseEntity.ok().body("file is not uploaded !");
 		}
-		
-							
-		
+
 	}
-	
-	
-	
+
+	@RequestMapping(value = "/addConsumers")
+	public ResponseEntity<?> addConsumers(@RequestBody Consumers consumers) {
+		LOGGER.info("From class ConsumersController, method : addConsumers() ");
+		this.piCode = consumers.getPiCode();
+		this.apiCode = consumers.getApiCode();
+		consumersRepository.save(consumers);
+		new Consumers(null, null, null, null, null, null, null, null, null, null, null);
+		return ResponseEntity.ok().body("your operation has been completed successfully");
+	}
+
 }
--- Poor-fund-rest-API/src/main/java/com/renu/pf_r_api/fileupload/FileUpload.java
+++ Poor-fund-rest-API/src/main/java/com/renu/pf_r_api/fileupload/FileUpload.java
@@ -12,33 +12,22 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class FileUpload {
 
-	private static final Path PI_ABS_PATH = Paths
-			.get("H:\\STS_Github\\Poor-fund-rest-API\\Poor-fund-rest-API\\src\\main\\resources\\static\\images\\");
-	private static final Path API_ABS_PATH = Paths
+	private static final Path ABS_PATH = Paths
 			.get("H:\\STS_Github\\Poor-fund-rest-API\\Poor-fund-rest-API\\src\\main\\resources\\static\\images\\");
 	private static final Logger LOGGER = LoggerFactory.getLogger(FileUpload.class);
 
-	public static void piFileUpload(MultipartFile piFile) {
+	public static void fileUpload(MultipartFile piFile, String piCode, MultipartFile apiFile, String apiCode) {
 
 		LOGGER.info("FROM piFileUpload method");
 
 		try {
-			Files.copy(piFile.getInputStream(), PI_ABS_PATH.resolve(piFile.getOriginalFilename()));
+			Files.copy(piFile.getInputStream(), ABS_PATH.resolve(piCode + ".jpg"));
+			Files.copy(apiFile.getInputStream(), ABS_PATH.resolve(apiCode + ".jpg"));
+
 		} catch (Exception e) {
 			throw new RuntimeException("FAIL!");
 		}
 
-	}
-
-	public static void apiFileUpload(MultipartFile apiFile, String apiCode) {
-
-		LOGGER.info("FROM apiFileUpload method");
-
-		try {
-			Files.copy(apiFile.getInputStream(), API_ABS_PATH.resolve(apiCode + ".jpg"));
-		} catch (Exception e) {
-			throw new RuntimeException("FAIL!");
-		}
 	}
 
 }
